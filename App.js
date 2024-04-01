7b0404ddc5fd570aefa9425aaa916a77eab18e13
Chaitanya Nalam <110450550+chaitanyanalam23@users.noreply.github.com>
2024-04-01 08:18:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Welcome to MP4 Stream Site</h2>
+     <video width="700px" height="400px" controls>
+      <source src="https://d2rtrcbcfufbc.cloudfront.net/video_2024-03-28_10-32-52.mp4" type="video/mp4" />
+     </video>
+     <video width="700px" height="400px" controls>
+      <source src="https://d2rtrcbcfufbc.cloudfront.net/Naatu+Naatu+Full+Video+Song+(Telugu)+%5B4K%5D+_+RRR+_+NTR%2CRam+Charan+_+MM+Keeravaani+_+SS+Rajamouli.mp4" type="video/mp4" />
+     </video>
+     <video width="700px" height="400px" controls>
+      <source src="https://d2rtrcbcfufbc.cloudfront.net/Kurchi+Madathapetti+Full+Video+Song+_+Guntur+Kaaram+_+Mahesh+Babu+_+Sreeleela+_+Trivikram+_+Thaman+S.mp4" type="video/mp4" />
+     </video>
+     <video width="700px" height="400px" controls>
+      <source src="https://d2rtrcbcfufbc.cloudfront.net/Chamkeela+Angeelesi+-+Video+Song+_+Dasara+_+Nani%2CKeerthy+Suresh+_+Santhosh+Narayanan_+Srikanth+Odela.mp4" type="video/mp4" />
+     </video>
+      
+    </div>
+  );
+}
+
+export default App;
